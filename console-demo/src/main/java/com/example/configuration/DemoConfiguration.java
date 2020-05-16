64a1a5bbcabbe392b4a86273589e1c3932756f00
Berenyi Lajos <berenyil03@gmail.com>
2020-05-16 13:28:55 +0200

    Creating class DemoConfiguration

---
+++ console-demo/src/main/java/com/example/configuration/DemoConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "com.example")
+@Configuration
+public class DemoConfiguration {
+
+
+}
